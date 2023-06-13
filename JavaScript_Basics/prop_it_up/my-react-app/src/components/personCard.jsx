afffd0522b6aa1f7707d7ca5a7a3d915518c921b
isaacsbm <isaacsonbm@gmail.com>
2023-06-12 17:15:11 -0700

    main

--- JavaScript_Basics/prop_it_up/my-react-app/src/components/personCard.jsx
+++ JavaScript_Basics/prop_it_up/my-react-app/src/components/personCard.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+        setCount(count + 1);
+    }
     return (
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {props.age + count}</p>
             <p>Hair Color: {props.hairColor} </p>
+            <button onClick={handleClick}>Birthday Button!</button>
         </div>
     )
 }
