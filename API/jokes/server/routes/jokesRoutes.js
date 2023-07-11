e4c9ede986fe1802fc72d4dcd22795a6950bf940
isaacsbm <isaacsonbm@gmail.com>
2023-07-10 18:08:05 -0700

    main

--- API/jokes/server/routes/jokesRoutes.js
+++ API/jokes/server/routes/jokesRoutes.js
@@ -2,8 +2,8 @@ const JokesController = require("../controllers/JokesControllers");
 
 module.exports = app => {
     app.get("/api/jokes", JokesController.getAllJokes);
-    app.post("/api/jokes", JokesController.createJoke);
     app.get("/api/jokes/:id", JokesController.getOneJoke);
+    app.post("/api/jokes", JokesController.createJoke);
     app.patch("/api/jokes/:id", JokesController.updateJoke);
     app.delete("/api/jokes/:id", JokesController.deleteForever);
 };
